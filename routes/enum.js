3757383649aa6a8ce147bace955a261529407af1
Lucas Soares <lucas.soares.araujo@hotmail.com>
2019-04-09 00:47:59 -0300

    feat: router enum. Closes #3

--- routes/enum.js
+++ routes/enum.js
@@ -1,25 +1,25 @@
 module.exports = app => {    
     const Enum = app.db.models.enum;
 
-    app.route("/enum")        
+    app.route("/enum")
         .get((req, res) => {
             Enum.findAll({where: req.query})
                 .then(enuns => {
-                    res.json({enum_list: enuns});
+                    res.json({lista: enuns});
                 })
                 .catch(error => {
                     res.status(412).json({msg: error.message});
                 });
         })
-        .post((req, res) => {
-            Enum.create(req.body)
-                .then(result => {
-                    res.status(201).json(result);
-                })
-                .catch(error => {
-                    res.status(412).json({msg: error.message})
-                });
-        });
+        // .post((req, res) => {
+        //     Enum.create(req.body)
+        //         .then(result => {
+        //             res.status(201).json(result);
+        //         })
+        //         .catch(error => {
+        //             res.status(412).json({msg: error.message})
+        //         });
+        // });
     
     app.route("/enum/:id")
         .get((req, res) => {            
@@ -34,25 +34,25 @@ module.exports = app => {
                 .catch(error => {
                     res.status(412).json({msg: error.message});
                 });                
-        })
-        .put((req, res) => {
-            Enum.update(req.body, {where: req.params})
-                .then(result => {
-                    console.log(req.body);
-                    res.sendStatus(204);
-                })
-                .catch(error => {
-                    res.status(412).json({msg: error.message});
-                });
-        })
-        .delete((req, res) => {
-            Enum.destroy({where: req.params})
-                .then(result => {
-                    res.sendStatus(204);
-                })
-                .catch(error => {
-                    res.status(412).json({msg: error.message});
-                });
         });
+        // .put((req, res) => {
+        //     Enum.update(req.body, {where: req.params})
+        //         .then(result => {
+        //             console.log(req.body);
+        //             res.sendStatus(204);
+        //         })
+        //         .catch(error => {
+        //             res.status(412).json({msg: error.message});
+        //         });
+        // })
+        // .delete((req, res) => {
+        //     Enum.destroy({where: req.params})
+        //         .then(result => {
+        //             res.sendStatus(204);
+        //         })
+        //         .catch(error => {
+        //             res.status(412).json({msg: error.message});
+        //         });
+        // });
 
 }
